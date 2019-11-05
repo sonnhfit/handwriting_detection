4e5fd7b9b41f37a3922ca78a7128a2bf00b91d0e
Nguyen Son <sonnguyen@tamdongtam.vn>
2019-11-05 20:47:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+﻿# django-deap-learning-mnist-sample
+
+ディープラーニング＋DjangoでMNIST(手書き数字画像認識)を実際に動かして試せるサンプルアプリです。
+
+解説：
+URL: https://sinyblog.com/django/ai_api_001/
+
+# 使い方
+
+1. 事前にpython(3.6以上)をインストールする。
+
+2. gitでプロジェクトをダウンロードする。
+
+```
+git clone https://github.com/sinjorjob/mnist_tutorial.git
+```
+
+※右上の「Clone or Download」→ 「Download ZIP」からダウンロードでもOKです。
+
+3. DOS画面よりセットアップコマンドを実行する。
+
+
+```
+cd django-mnist-master
+python -m venv env
+env\Scripts\activate
+pip install -r requirements.txt
+```
+
+4. マイグレーションを実行する。
+```
+python manage.py migrate
+```
+
+MacOS/Linuxでは動作確認していませんが、同様の手順で実装できると思います。
+
+5. 開発サーバを立ち上げてブラウザからURLを開く
+
+
+```
+python manage.py runserver
+```
+
+
+6. 以下のURLにアクセスする。
+
+```
+http://localhost:8000/mnist
+```
+
+7. 補足
+
+examplesフォルダ：kerasによるモデル定義～学習～モデル保存までのコード一式
+
+modelフォルダ：デモアプリ用の学習済みファイル
+
+sampleフォルダ：手書き数字画像のサンプルファイル
---
+++ mnist/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from PIL import Image
+import numpy as np
+import base64
+import tensorflow as tf
+import os
+
+from keras.models import load_model
+current_dir = os.getcwd()
+model_path = current_dir + '\\model\\mnist.h5'
+model = load_model(model_path)
+graph = tf.get_default_graph()
+
+
+def upload(request):
+      
+
+    files = request.FILES.getlist("files[]")
+
+
+    for memory_file in files:
+
+        root, ext = os.path.splitext(memory_file.name)
+     
+        if ext != '.jpg':
+            
+            message ="【ERROR】jpg nhập ảnh đúng định dạng"
+            return render(request, 'mnist/index.html', {
+                "message": message,
+                })
+
+
+    if request.method =='POST' and files:
+        result=[]
+        labels=[]
+        for file in files:
+            img = Image.open(file)
+            gray_img = img.convert('L')
+            img = gray_img.resize((28, 28))
+            img = np.array(img).reshape(1,28,28,1)
+            labels.append(mnist(img))
+        
+        for file, label in zip(files, labels):
+            file.seek(0)
+            file_name = file
+            src = base64.b64encode(file.read())
+            src = str(src)[2:-1]
+            result.append((src, label))
+
+        context = {
+            'result': result,
+        }
+        return render(request, 'mnist/result.html', context)
+    
+    else:
+        return redirect('index')
+
+
+def mnist(input):
+    
+    global graph
+    with graph.as_default():
+        result = (model.predict(input, batch_size=None, verbose=0, steps=None)).argmax()
+
+    return result
+    
